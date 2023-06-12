00db39a43a943ad348e60d4967c560d7636ec2a9
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-06-12 01:36:22 -0400

    Time: 15 ms (17.51%), Space: 44.5 MB (26.81%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        
+        int elementWithCountOne = -1; // Default value if no element with count 1 is found
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            int element = entry.getKey();
+            int count = entry.getValue();
+            if (count == 1) {
+                elementWithCountOne = element;
+                break;
+            }
+        }
+        return elementWithCountOne;
+    }
+}
