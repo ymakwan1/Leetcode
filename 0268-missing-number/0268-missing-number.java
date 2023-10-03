9df50d264290549ef52b129818ad73e0cfbbbea2
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-03 10:42:34 -0400

    Time: 0 ms (100.00%), Space: 44.1 MB (50.54%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,16 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        // Calculate the expected sum of first n+1 natural numbers.
+        
         int expectedSum = (n * (n + 1)) / 2;
         
-        // Calculate the actual sum of elements in the array.
+        
         int actualSum = 0;
         for (int num : nums) {
             actualSum += num;
         }
         
-        // The missing number is the difference between the expected sum and the actual sum.
+        
         return expectedSum - actualSum;
     }
 }
