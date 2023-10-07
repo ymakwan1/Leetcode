7ee5a7f5288457e71bb8e60df6d10885ed3defa7
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-06 23:46:58 -0400

    Time: 140 ms (72.06%), Space: 40.3 MB (88.23%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if(dfs(r,c,board,word,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean dfs(int r,int c,char[][] board, String word, int index){
+        if(index == word.length()){
+            return true;
+        }
+        
+        if(r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != word.charAt(index)){
+            return false;
+        }
+
+        
+
+        char temp = board[r][c];
+        board[r][c] = '.';
+
+        boolean found = dfs(r+1, c, board, word, index+1) ||
+                        dfs(r-1, c, board, word, index+1) ||
+                        dfs(r, c+1, board, word, index+1) ||
+                        dfs(r, c-1, board, word, index+1);
+        board[r][c] = temp;
+        return found;
+    }
+}
