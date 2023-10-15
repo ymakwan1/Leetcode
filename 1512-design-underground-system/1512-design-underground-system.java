a8cf752882bdef3ab089ec465bde957da5c72e01
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-15 18:24:10 -0400

    Time: 87 ms (83.23%), Space: 54.5 MB (51.02%) - LeetHub

--- 1512-design-underground-system/1512-design-underground-system.java
+++ 1512-design-underground-system/1512-design-underground-system.java
@@ -1,60 +1,67 @@
-import java.util.HashMap;
-
 class UndergroundSystem {
-    
-    private HashMap<Integer, CheckInData> checkInMap;
-    private HashMap<String, StationData> stationDataMap;
-    
+    HashMap<Integer, CheckInData> checkInMap;
+    HashMap<String, StationData> stationDataMap;
+
+    public static class CheckInData{
+        int id;
+        String stationName;
+        int t;
+        public CheckInData(int idIn, String stationNameIn, int tIn){
+            id = idIn;
+            stationName = stationNameIn;
+            t = tIn;
+        }
+    }
+
+    public static class StationData{
+        int totalTime;
+        int totalTrips;
+        
+        public StationData(int totalTimeIn, int totalTripsIn) {
+            totalTime = totalTimeIn;
+            totalTrips = totalTripsIn;
+        }
+    }
     public UndergroundSystem() {
         checkInMap = new HashMap<>();
         stationDataMap = new HashMap<>();
     }
     
     public void checkIn(int id, String stationName, int t) {
-        checkInMap.put(id, new CheckInData(stationName, t));
+        checkInMap.put(id, new CheckInData(id, stationName, t));
     }
     
     public void checkOut(int id, String stationName, int t) {
-        CheckInData checkInData = checkInMap.get(id);
-        String route = checkInData.stationName + "->" + stationName;
-        int travelTime = t - checkInData.time;
-        
-        if (!stationDataMap.containsKey(route)) {
+        CheckInData checkIn = checkInMap.get(id);
+        String route = checkIn.stationName + "->" + stationName;
+        int travelTime = t - checkIn.t;
+
+        if(!stationDataMap.containsKey(route)){
             stationDataMap.put(route, new StationData(0, 0));
         }
-        
+
         StationData stationData = stationDataMap.get(route);
         stationData.totalTime += travelTime;
         stationData.totalTrips++;
+
     }
     
     public double getAverageTime(String startStation, String endStation) {
         String route = startStation + "->" + endStation;
-        if (!stationDataMap.containsKey(route)) {
+        if(!stationDataMap.containsKey(route)){
             return 0.0;
         }
-        
+
         StationData stationData = stationDataMap.get(route);
+
         return (double) stationData.totalTime / stationData.totalTrips;
     }
-    
-    private static class CheckInData {
-        String stationName;
-        int time;
-        
-        public CheckInData(String stationName, int time) {
-            this.stationName = stationName;
-            this.time = time;
-        }
-    }
-    
-    private static class StationData {
-        int totalTime;
-        int totalTrips;
-        
-        public StationData(int totalTime, int totalTrips) {
-            this.totalTime = totalTime;
-            this.totalTrips = totalTrips;
-        }
-    }
 }
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * UndergroundSystem obj = new UndergroundSystem();
+ * obj.checkIn(id,stationName,t);
+ * obj.checkOut(id,stationName,t);
+ * double param_3 = obj.getAverageTime(startStation,endStation);
+ */
