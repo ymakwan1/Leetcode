0e163d9ed69b335805ace122051e0bcc19c29de5
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-15 12:29:39 -0400

    Time: 10 ms (5.32%), Space: 41.6 MB (6.28%) - LeetHub

---
+++ 1095-two-city-scheduling/1095-two-city-scheduling.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        Arrays.sort(costs, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                int diffA = a[0] - a[1];
+                int diffB = b[0] - b[1];
+                return Integer.compare(diffA, diffB);
+            }
+        });
+
+        int sum = 0;
+        for(int i = 0; i < costs.length/2; i++){
+            sum += costs[i][0];
+        }
+        for(int i = costs.length/2; i < costs.length; i++){
+            sum += costs[i][1];
+        }
+        System.out.println(sum);
+        // Print the sorted array
+        for (int[] row : costs) {
+            System.out.println(Arrays.toString(row));
+        }
+        return sum;
+    }
+}
