0843b37a36f2bcd5a1434f98d0ef28771e23b960
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-14 21:34:08 -0400

    Time: 9 ms (10.83%), Space: 50.8 MB (10.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,33 +1,34 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int noOfIslands = 0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j] == '1'){
-                    noOfIslands++;
-                    dfs(grid, i,j);
+        int count = 0;
+        boolean[][] boolgrid = new boolean[grid.length][grid[0].length];
+        int rows = grid.length;
+        int columns = grid[0].length;
+        int[][] directions = {{-1,0},{1,0},{0,-1},{0,1}};
+        for(int i = 0; i < rows; i++){
+            for(int j=0; j<columns; j++){
+                if(grid[i][j] == '1' && !boolgrid[i][j]){
+                    count++;
+                    Queue<int[]> queue = new LinkedList<>();
+                    queue.add(new int[]{i,j});
+                    while(!queue.isEmpty()){
+                        int[] current = queue.poll();
+                        int x = current[0];
+                        int y = current[1];
+
+                        for(int[] dir : directions){
+                            int newX = x + dir[0];
+                            int newY = y + dir[1];
+
+                            if(newX >=0 && newX<rows && newY>=0 && newY<columns && grid[newX][newY] == '1' && !boolgrid[newX][newY]){
+                                queue.add(new int[]{newX,newY});
+                                boolgrid[newX][newY] =true;
+                            }
+                        }
+                    }
                 }
             }
         }
-        return noOfIslands;
-    }
-    
-    private void dfs(char[][] grid, int r, int c){
-        grid[r][c] = 0;
-        if(r - 1 >= 0 && grid[r - 1][c] == '1'){
-            dfs(grid,r-1,c);
-        }
-        
-        if(r + 1 < grid.length && grid[r + 1][c] == '1'){
-            dfs(grid,r+1,c);
-        }
-        
-        if(c - 1 >= 0 && grid[r][c - 1] == '1'){
-            dfs(grid,r,c-1);
-        }
-        
-        if(c + 1 < grid[0].length && grid[r][c + 1] == '1'){
-            dfs(grid,r,c+1);
-        }
+        return count;
     }
 }
