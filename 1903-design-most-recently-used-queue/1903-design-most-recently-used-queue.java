db82df9ae2339b8721bbf89f74d31dc57858ba33
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-14 20:31:09 -0400

    Time: 28 ms (44.72%), Space: 44.1 MB (88.89%) - LeetHub

--- 1903-design-most-recently-used-queue/1903-design-most-recently-used-queue.java
+++ 1903-design-most-recently-used-queue/1903-design-most-recently-used-queue.java
@@ -1,21 +1,26 @@
 class MRUQueue {
     LinkedList<Integer> linkedList;
+    int[] mruQueue;
     public MRUQueue(int n) {
         linkedList = new LinkedList<>();
-
-        for(int i = 1; i <= n; i++){
+        mruQueue = new int[n];
+        for(int i = 0; i < n; i++){
             linkedList.add(i);
+            mruQueue[i] = i+1;
         }
     }
     
     public int fetch(int k) {
-        int toReturn = linkedList.get(k-1);
-        linkedList.remove(k-1);
-        linkedList.add(linkedList.size(), toReturn);
-        // for(int i = 0; i < linkedList.size(); i++){
-        //     System.out.println(linkedList.get(i));
-        // }
-        return toReturn;
+        int index = k-1;
+        int element = mruQueue[index];
+        for(int i=index; i < mruQueue.length - 1; i++){
+            mruQueue[i] = mruQueue[i+1];
+        }
+        mruQueue[mruQueue.length - 1] = element;
+        // int toReturn = linkedList.get(k-1);
+        // linkedList.remove(k-1);
+        // linkedList.add(linkedList.size(), toReturn);
+        return element;
     }
 }
 
