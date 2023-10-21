2480dd3663cfb997ba28dff5e3bb2c57fe1fa8e2
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-20 23:58:46 -0400

    Time: 4 ms (98.41%), Space: 43.6 MB (24.53%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -5,17 +5,13 @@ class Solution {
 
         for(char ins : moves.toCharArray()){
             if(ins == 'U'){
-                //x += 0;
                 y += 1;
             } else if(ins == 'D'){
-                //x += 0;
                 y -= 1;
             } else if(ins == 'R'){
                 x += 1;
-                //y += 0;
             } else if(ins == 'L'){
                 x -= 1;
-                //y += 0;
             }
         }
         return (x == 0 && y == 0);
