c8fea05c5980c4a6c0aa40f9271a2114eaf86390
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-20 23:30:47 -0400

    Time: 5 ms (56.46%), Space: 40.7 MB (69.72%) - LeetHub

--- 1283-reformat-date/1283-reformat-date.java
+++ 1283-reformat-date/1283-reformat-date.java
@@ -1,10 +1,28 @@
 class Solution {
     public String reformatDate(String date) {
+        Map<String, String> monthMap = new HashMap<>();
+        monthMap.put("Jan", "01");
+        monthMap.put("Feb", "02");
+        monthMap.put("Mar", "03");
+        monthMap.put("Apr", "04");
+        monthMap.put("May", "05");
+        monthMap.put("Jun", "06");
+        monthMap.put("Jul", "07");
+        monthMap.put("Aug", "08");
+        monthMap.put("Sep", "09");
+        monthMap.put("Oct", "10");
+        monthMap.put("Nov", "11");
+        monthMap.put("Dec", "12");
+
         String[] parts = date.split(" ");
         StringBuilder str = new StringBuilder();
-        String dates = handleDate(parts[0]);
-        String month = handleMonth(parts[1]);
-        str.append(parts[2]).append("-").append(month).append("-").append(dates);
+        str.append(parts[2]).append("-").append(monthMap.get(parts[1]));
+        String dates = parts[0].replaceAll("[a-zA-Z]", "");
+
+        if(dates.length() == 1){
+            dates = "0"+dates;
+        }
+        str.append("-").append(dates);
         return str.toString();
     }
 
