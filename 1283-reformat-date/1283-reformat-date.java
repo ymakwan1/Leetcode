23668a647710a9cd74de465d163c2ca81b515ec2
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-20 23:30:58 -0400

    Time: 5 ms (56.46%), Space: 40.1 MB (98.9%) - LeetHub

--- 1283-reformat-date/1283-reformat-date.java
+++ 1283-reformat-date/1283-reformat-date.java
@@ -25,34 +25,4 @@ class Solution {
         str.append("-").append(dates);
         return str.toString();
     }
-
-    public String handleDate(String dates){
-        String result = dates.replaceAll("[a-zA-Z]", "");
-        if(result.length() == 1){
-            return "0"+result;
-        }
-        return result;
-    }
-
-    public String handleMonth(String month){
-        Map<String, Integer> monthMap = new HashMap<>();
-        monthMap.put("Jan", 1);
-        monthMap.put("Feb", 2);
-        monthMap.put("Mar", 3);
-        monthMap.put("Apr", 4);
-        monthMap.put("May", 5);
-        monthMap.put("Jun", 6);
-        monthMap.put("Jul", 7);
-        monthMap.put("Aug", 8);
-        monthMap.put("Sep", 9);
-        monthMap.put("Oct", 10);
-        monthMap.put("Nov", 11);
-        monthMap.put("Dec", 12);
-        
-        if(String.valueOf(monthMap.get(month)).length() == 1){
-            return "0"+String.valueOf(monthMap.get(month));
-        }
-
-        return String.valueOf(monthMap.get(month));
-    }
 }
