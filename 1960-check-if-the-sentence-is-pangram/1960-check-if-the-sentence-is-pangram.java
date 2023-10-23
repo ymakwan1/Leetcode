ca044305ef623f2226ac0dc37007ad68b53c4429
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-23 14:46:57 -0400

    Time: 3 ms (54.2%), Space: 40.4 MB (67%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -1,17 +1,25 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        int[] map = new int[26];
-
+        // int[] map = new int[26];
+Set<Character> set = new HashSet<>();
         for(char s : sentence.toCharArray()){
-            map[s - 'a'] = map[s-'a'] + 1;
+            //map[s - 'a'] = map[s-'a'] + 1;
+            set.add(s);
         }
 
-        for(int i = 0; i < map.length; i++){
-            if(map[i] == 0){
-                return false;
-            }
+        if(set.size() == 26){
+            return true;
         }
+        return false;
 
-        return true;
+        // for(int i = 0; i < map.length; i++){
+        //     if(map[i] == 0){
+        //         return false;
+        //     }
+        // }
+
+        // return true;
+        
+
     }
 }
