0ad9145c46050f1a2c5c0ee5efaa7e68117cf317
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-24 11:06:19 -0400

    Time: 1 ms (78.04%), Space: 40.4 MB (75.89%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.java
+++ 0874-backspace-string-compare/0874-backspace-string-compare.java
@@ -8,40 +8,56 @@ class Solution {
         StringBuilder tRes = new StringBuilder();
 
         for(char c : s.toCharArray()){
-            if(!stackS.isEmpty()){
-                if(c == '#'){
-                    stackS.pop();
-                } else if(c != '#') {
-                    stackS.push(c);
+            // if(!stackS.isEmpty()){
+            //     if(c == '#'){
+            //         stackS.pop();
+            //     } else if(c != '#') {
+            //         stackS.push(c);
+            //     }
+            // }else if(c != '#'){
+            //     stackS.push(c);
+            // }
+
+            if(c == '#'){
+                if(sRes.length() > 0){
+                    sRes.deleteCharAt(sRes.length() - 1);
                 }
-            }else if(c != '#'){
-                stackS.push(c);
+            }else {
+                    sRes.append(c);
             }
         }
 
         for(char c : t.toCharArray()){
-            if(!stackT.isEmpty()){
-                if(c == '#'){
-                    stackT.pop();
-                } else if(c != '#') {
-                    stackT.push(c);
-                }
-            }else if(c != '#'){
-                stackT.push(c);
+            // if(!stackT.isEmpty()){
+            //     if(c == '#'){
+            //         stackT.pop();
+            //     } else if(c != '#') {
+            //         stackT.push(c);
+            //     }
+            // }else if(c != '#'){
+            //     stackT.push(c);
+            // }
+            if(c == '#'){
+                if(tRes.length() > 0){
+                    tRes.deleteCharAt(tRes.length() - 1);
+                } 
+            }else {
+                    tRes.append(c);
             }
         }
 
-        while(!stackS.isEmpty()){
-            sRes.append(stackS.pop());
-        }
+        // while(!stackS.isEmpty()){
+        //     sRes.append(stackS.pop());
+        // }
 
-        while(!stackT.isEmpty()){
-            tRes.append(stackT.pop());
-        }
+        // while(!stackT.isEmpty()){
+        //     tRes.append(stackT.pop());
+        // }
 
-        if(sRes.toString().equals(tRes.toString())){
-            return true;
-        }
-        return false;
+        // if(sRes.toString().equals(tRes.toString())){
+        //     return true;
+        // }
+        // return false;
+        return sRes.toString().compareTo(tRes.toString()) == 0;
     }
 }
