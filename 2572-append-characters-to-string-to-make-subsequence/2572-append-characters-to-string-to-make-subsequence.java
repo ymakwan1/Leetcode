753c421dea8e7f599788ddf3e37be1c6f75b1566
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-10-24 22:42:07 -0400

    Time: 6 ms (63.84%), Space: 44.2 MB (45.44%) - LeetHub

---
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int i = 0;
+        int j = 0;
+        
+        if(t.length() == 1 && s.contains(t)){
+            return 0;
+        }
+
+        while(i < s.length() && j < t.length()){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return t.length() - j;
+    }
+}
