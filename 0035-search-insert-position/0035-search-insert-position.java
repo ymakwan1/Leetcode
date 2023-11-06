66ad98cbd172089f7cc4db67984d08994d9a6cee
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2023-11-06 15:38:03 -0500

    Time: 0 ms (100%), Space: 43.3 MB (28.96%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,21 +1,16 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int num : nums) {
-            arrayList.add(num);
-        }
-        
-        if(arrayList.contains(target)) {
-            return arrayList.indexOf(target);
-        } else if (target > arrayList.get(arrayList.size() - 1)) {
-            return arrayList.size();
-        } else {
-            for(int i = 0; i < arrayList.size(); i++) {
-                if (!(arrayList.get(i) < target)) {
-                    return i;
-                }
+        int low = -1;
+        int high = nums.length;
+
+        while(low + 1 < high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] >= target){
+                high = mid;
+            }else{
+                low = mid;
             }
         }
-        return 0;
+        return high;
     }
 }
