38542207926796159e19857f0609128b6e98eff2
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-04-02 23:31:30 -0400

    Time: 2 ms (84.78%) | Memory: 45.8 MB (81.55%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -5,6 +5,9 @@ class Solution {
         for (int end = 0; end < nums.length; end++) {
             if (nums[end] != 0) {
                 nums[start] = nums[end];
+                if (start != end) {
+                    nums[end] = 0; 
+                }
                 start++;
             }
         }
