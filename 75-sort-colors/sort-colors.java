f3010dab35dd1b4d0b45a81d5b5f988fe4ac5028
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-04-03 21:57:41 -0400

    Time: 1 ms (23.14%) | Memory: 41.7 MB (50.64%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int n = nums.length;
+        
+        // Iterate over each element in the array starting from the second one
+        for (int i = 1; i < n; i++) {
+            int key = nums[i];
+            int j = i - 1;
+            
+            // Move elements of array[0..i-1], that are greater than key, to one position ahead
+            // of their current position
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j = j - 1;
+            }
+            nums[j + 1] = key;
+        }
     }
 }
