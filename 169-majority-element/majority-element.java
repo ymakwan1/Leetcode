2938644a4d01883fbdc4dadecc56352a75a0fc08
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-04-03 22:49:07 -0400

    Time: 13 ms (27.69%) | Memory: 49.2 MB (77.49%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        int key=0;
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        for(Map.Entry<Integer, Integer> m : map.entrySet()){
+            if(m.getValue() > count){
+                // System.out.println(m.getValue());
+                // System.out.println(count);
+                count = m.getValue();
+                key = m.getKey();
+                // System.out.println("updated"+count);
+            }
+            // System.out.println(m.getKey() +":"+m.getValue());
+        }
+        System.out.println(count);
+        return (key);
+    }
+}
