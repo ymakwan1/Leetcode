47940716fa234e55abb8646b392486dbbd50d6f0
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-04-09 22:28:27 -0400

    Time: 0 ms (100.00%) | Memory: 50.4 MB (36.74%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -4,20 +4,25 @@ class Solution {
         if(n == 1){
             return nums[0];
         }
+        if(nums[0] != nums[1]){
+            return nums[0];
+        }
+        if(nums[n-2] != nums[n-1]){
+            return nums[n-1];
+        }
+        int low = 1;
+        int high = n - 2;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }
 
-        for(int i = 0; i < n; i++){
-            if(i == 0){
-                if(nums[i] != nums[i+1]){
-                    return nums[i];
-                }
-            } else if (i == n - 1){
-                if(nums[i] != nums[i-1]){
-                    return nums[i];
-                }
-            } else {
-                if(nums[i] != nums[i-1] && nums[i] != nums[i+1]){
-                    return nums[i];
-                }
+            //in left half check
+            if((mid % 2 == 0 && (nums[mid] == nums[mid+1])) || (mid % 2 == 1 && (nums[mid] == nums[mid-1]))){
+                low = mid + 1;
+            }else {
+                high = mid - 1;
             }
         }
         return 0;
