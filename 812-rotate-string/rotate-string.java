98dbebcb9687899d7ad631d8bd366be37e550635
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-04-14 20:08:27 -0400

    Time: 0 ms (100.00%) | Memory: 41.1 MB (85.08%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,10 +1,14 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+
+        //Check if length same, or else cannot be rotated
         if(s.length() != goal.length())
             return false;
 
+        //Concate s with itself
         String str = s + s;
-        //System.out.println(str);
+        
+        //Check if goal is substring
         if(str.contains(goal)){
             return true;
         }
