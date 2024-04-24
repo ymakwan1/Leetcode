93eb38461aec452d97b1c92b0328931ef47c256e
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-04-23 23:36:03 -0400

    Time: 1 ms (100.00%) | Memory: 44.2 MB (64.04%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,54 +10,32 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carryFlag = 0;
+        
+        ListNode dummy = new ListNode();
+        ListNode temp = dummy;
+
         int carry = 0;
-        int sum = 0 ;
-        ListNode dummy = new ListNode(0);
-        ListNode t = dummy;
-        
-        while(l1 != null && l2 != null){
-            if(carryFlag == 1){
-                sum = l1.val + l2.val + carry;
-                carryFlag = (sum > 9) ? 1 : 0;
-                carry = (sum > 9) ? sum / 10 : 0;
-                sum = (sum > 9) ? sum % 10 : sum;
-                dummy.next  = new ListNode(sum);
-            } else{
-                sum = l1.val + l2.val;
-                carryFlag = (sum > 9) ? 1 : 0;
-                carry = (sum > 9) ? sum / 10 : 0;
-                sum = (sum > 9) ? sum % 10 : sum;
-                dummy.next  = new ListNode(sum);
+        while(l1 != null || l2 != null || carry == 1){
+            int sum = 0;
+
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
             }
-            l1 = l1.next;
-            l2 = l2.next;
-            dummy = dummy.next;
+
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            sum += carry;
+            carry = sum / 10;
+
+            ListNode node = new ListNode(sum % 10);
+            temp.next = node;
+            temp = temp.next;
         }
-        
-        while(l1 != null){
-            sum = l1.val + carry;
-                carryFlag = (sum > 9) ? 1 : 0;
-                carry = (sum > 9) ? sum / 10 : 0;
-                sum = (sum > 9) ? sum % 10 : sum;
-            l1 = l1.next;
-            dummy.next  = new ListNode(sum);
-            dummy = dummy.next;
-        }
-        
-        while(l2 != null){
-            sum = l2.val + carry;
-            carryFlag = (sum > 9) ? 1 : 0;
-            carry = (sum > 9) ? sum / 10 : 0;
-            sum = (sum > 9) ? sum % 10 : sum;
-            l2 = l2.next;
-            dummy.next  = new ListNode(sum);
-            dummy = dummy.next;
-        }
-        if(carry != 0){
-            dummy.next  = new ListNode(carry);
-        }
-        
-        return t.next;
+
+        return dummy.next;
     }
 }
