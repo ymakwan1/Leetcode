a63049660ba113c9995aa2e403ed14a47f046f07
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-04-29 00:55:10 -0400

    Time: 498 ms (67.08%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,12 +1,7 @@
 -- Write your PostgreSQL query statement below
-WITH duplicates AS (
-    SELECT email, id, ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS row_num
-    FROM Person
-)
-
 DELETE FROM Person
-WHERE id in (
-    SELECT id 
-    FROM duplicates
-    WHERE row_num > 1
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM Person
+    GROUP BY email
 );
