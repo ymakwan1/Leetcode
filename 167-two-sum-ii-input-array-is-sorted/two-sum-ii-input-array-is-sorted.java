ee7f08bde70384503a2d7da3049ca71c543b431d
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-04-28 20:44:32 -0400

    Time: 2 ms (93.11%) | Memory: 47.4 MB (15.01%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -4,14 +4,12 @@ class Solution {
         int start = 0;
         int end = n - 1;
         int sum = 0;
-        ArrayList<Integer> res = new ArrayList<>();
+
         int[] result = new int[2];
 
         while(start <= end){
             sum = numbers[start] + numbers[end];
             if(sum == target){
-                // res.add(start);
-                // res.add(end);
                 result[0] = start+1;
                 result[1] = end+1;
                 break;
@@ -22,8 +20,6 @@ class Solution {
             }
         }
 
-        // int[] arr = new int[res.size()];
-        // arr = al.toArray(arr);
         return result;
     }
 }
