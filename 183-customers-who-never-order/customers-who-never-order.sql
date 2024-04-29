76b026c7c116493e74dc667108fbbb78318941e5
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-04-29 00:13:24 -0400

    Time: 467 ms (65.22%) | Memory: 0B (100.00%) - LeetSync

---
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+SELECT c.name AS "Customers"
+FROM Customers c
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Orders o
+    WHERE c.id = o.customerId
+);
