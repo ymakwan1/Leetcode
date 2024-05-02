c7b031d865aedbd04e70dd1744724e5c712f97bc
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-05-02 15:36:22 -0400

    Time: 4 ms (100.00%) | Memory: 43.6 MB (90.57%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        boolean isNegative = false;
+        
+        if(x < 0){
+            return false;
+        }
+
+        int reverse = 0; 
+        int temp = x; 
+
+        while (temp != 0) { 
+            reverse = (reverse * 10) + (temp % 10); 
+            temp = temp / 10; 
+        }
+
+        return (reverse == x);
+    }
+}
