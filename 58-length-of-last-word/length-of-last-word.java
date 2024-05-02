16af2e9ab649951e110104ab66214ea3ed252299
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-05-02 15:45:56 -0400

    Time: 1 ms (31.04%) | Memory: 41.6 MB (40.74%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,13 +1,9 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        char[] c = s.toCharArray();
+     
         s=s.trim();
         String strArray[] = s.split(" ");
-        for(String st : strArray){
-            //System.out.println(st);
-        }
-        System.out.println(strArray[strArray.length - 1].length());
-       // System.out.println(s.split(" "));
+        
         return strArray[strArray.length - 1].length();
     }
 }
