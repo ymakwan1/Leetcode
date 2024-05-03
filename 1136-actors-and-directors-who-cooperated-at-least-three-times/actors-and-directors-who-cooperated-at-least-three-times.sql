7336d01c3e35ccb3db700f42c47781ec45aa1734
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-05-02 23:46:01 -0400

    Time: 594 ms (70.45%) | Memory: 0B (100.00%) - LeetSync

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,8 +1,9 @@
 -- Write your PostgreSQL query statement below
-SELECT actor_id, director_id
-FROM (
-    SELECT actor_id, director_id, COUNT(*) AS collaborations
+WITH Collaborations AS (
+    SELECT actor_id, director_id, COUNT(*) AS num_collaborations
     FROM ActorDirector
     GROUP BY actor_id, director_id
-) AS ad
-WHERE ad.collaborations >= 3;
+)
+SELECT actor_id, director_id
+FROM Collaborations
+WHERE num_collaborations >= 3;
