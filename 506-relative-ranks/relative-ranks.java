1bbf73f97b4e95d7c4a7ca41e5955aa4ff757d79
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-05-07 22:18:16 -0400

    Time: 62 ms (13.32%) | Memory: 45.7 MB (28.80%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String[] findRelativeRanks(int[] score) {
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < score.length; i++) {
+            result.add(""); // Add empty strings initially
+        }
+        int i = 4;
+        for(int j = 0; j < score.length; j++){
+            int k = findMax(score);
+            //System.out.println(k);
+            if(j==0){
+                result.set(k, "Gold Medal");
+            } else if(j == 1){
+                result.set(k, "Silver Medal");
+            }else if (j== 2){
+                result.set(k, "Bronze Medal");
+            }else{
+                result.set(k, String.valueOf(j+1));
+            }
+            score[k] = -1;
+        }
+
+        return result.toArray(new String[0]);
+    }
+
+    private int findMax(int[] score){
+        int index = 0;
+        for (int i = 1; i < score.length; i++) {
+            if (score[i] > score[index]) {
+                //System.out.println("here");
+                index = i;
+            }
+        }
+        return index;
+    }
+}
