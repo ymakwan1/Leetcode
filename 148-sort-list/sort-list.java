7802b07195ed72f5329a2969201e5246ff47404e
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-05-08 14:03:32 -0400

    Time: 9 ms (93.79%) | Memory: 57.3 MB (12.19%) - LeetSync

--- 148-sort-list/sort-list.java
+++ 148-sort-list/sort-list.java
@@ -10,28 +10,45 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        if(head == null){
-            return null;
+        if(head == null || head.next == null){
+            return head;
         }
 
-        ArrayList<Integer> numlist= new ArrayList<>();
-        ListNode curr = head;
+        ListNode prev = null;
+        ListNode fast = head;
+        ListNode slow = head;
 
-        while(curr != null){
-            numlist.add(curr.val);
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2){
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                curr.next = l1;
+                l1 = l1.next;
+            }else{
+                curr.next = l2;
+                l2 = l2.next;
+            }
             curr = curr.next;
         }
 
-        Collections.sort(numlist);
-
-        ListNode dummy = new ListNode(-1);
-        ListNode p = dummy;
-        for(int i = 0; i < numlist.size(); i++){
-            ListNode temp = new ListNode(numlist.get(i));
-            p.next = temp;
-            p = p.next;
-        }
-
+        curr.next = (l1 != null) ? l1 : l2;
+        
         return dummy.next;
     }
 }
