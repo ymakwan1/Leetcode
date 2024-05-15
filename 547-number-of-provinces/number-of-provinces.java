9eaf2d80f89306284dbebbf066b99d8c5ac65f99
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-05-15 03:33:46 -0400

    Time: 0 ms (100.00%) | Memory: 48.1 MB (11.71%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -16,6 +16,8 @@ class Solution {
     }
 
     private void dfs(int[][] isConnected, boolean[] visited, int city){
+        if(visited[city])
+            return;
         visited[city] = true;
         for(int n = 0; n < isConnected.length; n++){
             if(isConnected[city][n] == 1 && !visited[n]){
