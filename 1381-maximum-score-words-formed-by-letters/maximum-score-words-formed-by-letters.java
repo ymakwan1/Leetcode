414409f1758c2e6db48a9c56ece6635dd130e956
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-05-24 15:37:11 +0530

    Time: 1 ms (81.78%) | Memory: 42 MB (36.02%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.java
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.java
@@ -1,31 +1,30 @@
 class Solution {
     public int maxScoreWords(String[] words, char[] letters, int[] score) {
-
-        // freq for available letters
-        int[] letterCounts = new int[26];
-        for(char l : letters){
-            letterCounts[l-'a']++;
+        // frequency array for the available letters
+        int[] letterCount = new int[26];
+        for (char letter : letters) {
+            letterCount[letter - 'a']++;
         }
-
-        //backtrack
-        return backtrack(words, letterCounts, score, 0);
+        
+        // backtracking to find the maximum score
+        return backtrack(words, letterCount, score, 0, 0);
     }
 
-    private int backtrack(String[] words, int[] letterCount, int[] score, int index){
-        if(index == words.length){
-            return 0;
+    private int backtrack(String[] words, int[] letterCount, int[] score, int index, int currentScore) {
+        if (index == words.length) {
+            return currentScore;
         }
 
-        //Skip current word
-        int maxScore = backtrack(words, letterCount, score, index+1);
+        // Skip the current word
+        int maxScore = backtrack(words, letterCount, score, index + 1, currentScore);
 
-        //Use current word
+        // use the current word
         String word = words[index];
         int wordScore = 0;
         boolean canUseWord = true;
         int[] tempCount = Arrays.copyOf(letterCount, letterCount.length);
 
-        for(char c : word.toCharArray()){
+        for (char c : word.toCharArray()) {
             if (tempCount[c - 'a']-- <= 0) {
                 canUseWord = false;
                 break;
@@ -33,8 +32,8 @@ class Solution {
             wordScore += score[c - 'a'];
         }
 
-        if(canUseWord){
-            maxScore = Math.max(maxScore, wordScore + backtrack(words, tempCount, score, index+1));
+        if (canUseWord) {
+            maxScore = Math.max(maxScore, backtrack(words, tempCount, score, index + 1, currentScore + wordScore));
         }
 
         return maxScore;
