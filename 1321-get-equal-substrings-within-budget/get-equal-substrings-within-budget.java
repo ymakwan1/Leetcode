048c1485153a63894fa55ee1105d7540db44c5ed
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-05-28 11:33:58 +0530

    Time: 5 ms (97.52%) | Memory: 43.2 MB (40.06%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
@@ -1,21 +1,24 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
+
         int n = s.length();
         int left = 0, right = 0;
-        int currentCost = 0;
         int maxLength = 0;
+        int currentCost = 0;
+        int[] cost = new int[n];
+
+        for(int i= 0; i < n; i++){
+            cost[i] = Math.abs(s.charAt(i)-t.charAt(i));
+        }
 
         while (right < n) {
-            // Add the cost of converting s[right] to t[right]
-            currentCost += Math.abs(s.charAt(right) - t.charAt(right));
+            currentCost += cost[right];
 
-            // If the currentCost exceeds the maxCost, move the left pointer to the right
             while (currentCost > maxCost) {
-                currentCost -= Math.abs(s.charAt(left) - t.charAt(left));
+                currentCost -= cost[left];
                 left++;
             }
 
-            // Calculate the length of the current window and update maxLength
             maxLength = Math.max(maxLength, right - left + 1);
             right++;
         }
