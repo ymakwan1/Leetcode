c0302406e0a021a2399b43163c65df2045d438d2
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-06-12 16:56:10 +0530

    Time: 13 ms (30.35%), Space: 44.9 MB (6.61%) - LeetHub

---
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int maxLen = 0;
+        int[] hash = new int[1000001];
+        int left = 0;
+        int right = 0;
+        int n = s.length();
+        int uniqueCount = 0;
+
+        while(right < n){
+
+            if(hash[s.charAt(right)] == 0){
+                uniqueCount++;
+            }
+            hash[s.charAt(right)]++;
+
+            while (uniqueCount > k) {
+                hash[s.charAt(left)]--;
+                if (hash[s.charAt(left)] == 0) {
+                    uniqueCount--;
+                }
+                left++;
+            }
+
+            maxLen = Math.max(maxLen, right - left + 1);
+            right++;
+        }
+
+        return maxLen;
+    }
+}
