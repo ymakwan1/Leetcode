8c5a2e8b2404b4f61bfb45046817c03ef75d7508
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-06-15 11:07:40 +0530

    Time: 621 ms (5.06%), Space: 64.3 MB (13.08%) - LeetHub

---
+++ 0502-ipo/0502-ipo.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int n = profits.length;
+
+        int[][] projects = new int[n][2];
+
+        for(int i = 0; i < n; i++){
+            projects[i][0] = capital[i];
+            projects[i][1] = profits[i];
+        }
+
+        Arrays.sort(projects, Comparator.comparingInt(a -> a[0]));
+        for(int i = 0; i < n; i++){
+            System.out.println(projects[i][0] + " " + projects[i][1]);
+        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int index = 0;
+        for(int i = 0; i < k; i++){
+            while (index < n && projects[index][0] <= w) {
+                pq.add(projects[index][1]);
+                index++;
+            }
+
+            if (pq.isEmpty()) {
+                break;
+            }
+
+            w += pq.poll();
+        }
+
+        return w;
+    }
+}
