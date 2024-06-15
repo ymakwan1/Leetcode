212a894aad82d0909d5ba3992f1fd43f25c85e0d
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-06-15 13:03:22 +0530

    Time: 2 ms (54.81%), Space: 41.6 MB (26.47%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,24 +1,23 @@
-import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
-        char[] c = s.toCharArray();
-        Stack<Character> stack = new Stack<Character>();
-        
-        for (int i = 0; i < c.length; i++) {
-            if (c[i] == '[' || c[i] == '{' || c[i] == '(') {
-                stack.push(c[i]);
-            } else if (c[i] == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
-            } else if (c[i] == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
-            } else if (c[i] == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else {
-                return false;
+        HashMap<Character, Character> map = new HashMap<>();
+
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if (map.containsValue(ch)) {
+                stack.push(ch);
+            } else if (map.containsKey(ch)){
+                if (stack.isEmpty() || map.get(ch) != stack.pop()) {
+                    return false;
+                }
             }
         }
-        
+
         return stack.isEmpty();
     }
 }
