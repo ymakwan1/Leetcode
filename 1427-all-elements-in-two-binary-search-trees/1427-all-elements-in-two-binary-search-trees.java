2cb7b2ec4d4b348bf341d94a8372f47d8d5ccb20
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-07-01 21:34:34 -0400

    Time: 21 ms (22.94%), Space: 46.1 MB (61.68%) - LeetHub

---
+++ 1427-all-elements-in-two-binary-search-trees/1427-all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        dfs(root1, list1);
+        List<Integer> list2 = new ArrayList<>();
+        dfs(root2, list2);
+
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0; i < list1.size(); i++){
+            result.add(list1.get(i));
+        }
+
+        for(int i = 0; i < list2.size(); i++){
+            result.add(list2.get(i));
+        }
+
+        Collections.sort(result);
+
+        return result;
+
+    }
+
+    private void dfs(TreeNode node, List<Integer> resList){
+        if(node == null){
+            return;
+        }
+
+        dfs(node.left, resList);
+        dfs(node.right, resList);
+
+        resList.add(node.val);
+    }
+}
