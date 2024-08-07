0c5360d33100e1d8fc4c5041560fa7c1e601a30d
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-08-07 17:46:18 -0400

    Time: 13 ms (9.87%), Space: 42.8 MB (8.29%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,43 +1,30 @@
 class Solution {
-    public String numberToWords(int num) {
-    if(num == 0)
-        return "Zero";
-    String[] bigString = new String[]{"Thousand","Million","Billion"};
-    String result =  numberToWordsHelper(num%1000);
-    num = num/1000;
-    if(num > 0 && num%1000>0){
-        result = numberToWordsHelper(num%1000) + "Thousand " + result;
-    }
-    num = num/1000;
-    if(num > 0 && num%1000>0){
-        result = numberToWordsHelper(num%1000) + "Million " + result;
-    }
-    num = num/1000;
-    if(num > 0){
-        result = numberToWordsHelper(num%1000) + "Billion " + result;
-    }
-    return result.trim();
-}
+    private static final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", 
+                                                  "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    private static final String[] TENS = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private static final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
 
-public String numberToWordsHelper(int num){
-    String[] digitString = new String[]{"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
-    String[] teenString = new String[]{"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen","Eighteen", "Nineteen"};
-    String[] tenString = new String[]{"","","Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-    String result = "";
-    if(num > 99){
-        result += digitString[num/100] + " Hundred ";
-    }
-    num = num % 100;
-    if(num < 20 && num > 9){
-        result += teenString[num%10]+" ";
-    }else{
-        if(num > 19){
-            result += tenString[num/10]+" ";
+    public String numberToWords(int num) {
+        if (num == 0) return "Zero";
+
+        int i = 0;
+        String words = "";
+
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                words = helper(num % 1000) + THOUSANDS[i] + " " + words;
+            }
+            num /= 1000;
+            i++;
         }
-        num = num % 10;
-        if(num > 0)
-            result += digitString[num]+" ";
+
+        return words.trim();
     }
-    return result;
-}
+
+    private String helper(int num) {
+        if (num == 0) return "";
+        else if (num < 20) return LESS_THAN_20[num] + " ";
+        else if (num < 100) return TENS[num / 10] + " " + helper(num % 10);
+        else return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+    }
 }
