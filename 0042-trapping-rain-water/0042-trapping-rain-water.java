b40fa033c31081a23441c67437c8f09535db5083
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-12-21 17:38:36 -0500

    Time: 1 ms (65.1%), Space: 46.4 MB (46.94%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,33 +1,28 @@
 class Solution {
     public int trap(int[] height) {
-        if(height == null || height.length == 0){
-            return 0;
+        int n = height.length;
+        int total = 0, leftMax = 0, rightMax = 0;
+        int[] suffix = new int[n];
+        int[] prefix = new int[n];
+
+        prefix[0] = height[0];
+        suffix[n-1] = height[n-1];
+
+        for(int i = 1; i < n; i++){
+            prefix[i] = Math.max(prefix[i-1], height[i]);
+        }
+        for(int i = n-2; i >= 0; i--){
+            suffix[i] = Math.max(suffix[i+1], height[i]);
         }
 
-        int left = 0;
-        int right = height.length - 1;
-        int leftMax = 0;
-        int rightMax = 0;
-        int waterTrapped = 0;
-
-        while(left < right){
-            if(height[left] < height[right]){
-                if(height[left] >= leftMax){
-                    leftMax = height[left];
-                }else{
-                    waterTrapped += leftMax - height[left];
-                }
-                left++;
-            }else{
-                if(height[right] >= rightMax){
-                    rightMax = height[right];
-                }else{
-                    waterTrapped += rightMax - height[right];
-                }
-                right--;
+        for(int i = 0; i < n; i++){
+            leftMax = prefix[i];
+            rightMax = suffix[i];
+            if(height[i] < leftMax && height[i] < rightMax){
+                total += Math.min(leftMax, rightMax) - height[i];
             }
         }
 
-        return waterTrapped;
+        return total;
     }
 }
