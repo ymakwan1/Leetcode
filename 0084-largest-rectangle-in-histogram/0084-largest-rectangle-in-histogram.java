783146ac5323605ef96d24ecccb7566f0643fb61
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-12-22 18:15:55 -0500

    Time: 66 ms (60.64%), Space: 57.9 MB (25.47%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,46 +1,30 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] nse = findNSE(heights);
-        int[] pse = findPSE(heights);
         int n = heights.length;
-        int max = 0;
+        int maxArea = 0;
+        Stack<Integer> stack = new Stack<Integer>();
 
         for(int i = 0; i < n; i++){
-            max = Math.max(max, (heights[i] * (nse[i] - pse[i] - 1)));
-        }
-
-        return max;
-    }
-
-    private int[] findNSE(int[] arr) {
-        int n = arr.length;
-        int[] nse = new int[n];
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+            while(!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+                int elem = stack.peek();
                 stack.pop();
+                int nse = i;
+                int pse = stack.isEmpty() ? -1 : stack.peek();
+
+                maxArea = Math.max(heights[elem] * (nse - pse - 1), maxArea);
             }
-            nse[i] = stack.isEmpty() ? n : stack.peek();
             stack.push(i);
         }
 
-        return nse;
-    }
+        while (!stack.isEmpty()) {
+            int elem = stack.pop();
+            int nse = n;
+            int pse = stack.isEmpty() ? -1 : stack.peek();
 
-    private int[] findPSE(int[] arr) {
-        int n = arr.length;
-        int[] pse = new int[n];
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
-                stack.pop();
-            }
-            pse[i] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(i);
+            maxArea = Math.max(heights[elem] * (nse - pse - 1), maxArea);
         }
 
-        return pse;
+
+        return maxArea;
     }
 }
