9fed40d947c0575b18ead86b24a24b9478f037b0
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2024-12-28 13:45:23 -0500

    Time: 3 ms (77.75%), Space: 43.3 MB (40.28%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,21 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(candidates);  // Sort to handle duplicates and to make the logic of skipping duplicates easier
-        backtrack(result, new ArrayList<>(), candidates, target, 0);
+        List<Integer> current = new ArrayList<>();
+        int n = candidates.length;
+        Arrays.sort(candidates);
+        subSum(0, candidates, n, result, current, target);
         return result;
     }
 
-    private void backtrack(List<List<Integer>> result, List<Integer> tempList, int[] candidates, int remain, int start) {
-        if (remain == 0) {
-            result.add(new ArrayList<>(tempList));  // Found a valid combination
-        } else if (remain > 0) {
-            for (int i = start; i < candidates.length; i++) {
-                if (i > start && candidates[i] == candidates[i - 1]) continue;  // Skip duplicates
-                tempList.add(candidates[i]);
-                backtrack(result, tempList, candidates, remain - candidates[i], i + 1);  // Move to the next index
-                tempList.remove(tempList.size() - 1);  // Backtrack
+    private void subSum(int index, int[] candidates, int n, List<List<Integer>> result, List<Integer> current,int target){
+        if(target == 0){
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for(int i = index; i < n; i++){
+            if (i > index && candidates[i] == candidates[i - 1]) {
+                continue;
             }
+            if (candidates[i] > target) {
+                break;
+            }
+            current.add(candidates[i]);
+            subSum(i + 1, candidates, n, result, current, target - candidates[i]);
+            current.remove(current.size() - 1);
         }
     }
 }
