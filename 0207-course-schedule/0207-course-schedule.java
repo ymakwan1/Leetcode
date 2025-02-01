4b59ec54a01d59c94eac98ca51825c3ca264a0a8
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-02-01 15:16:00 -0500

    Time: 6 ms (74.24%), Space: 45.6 MB (42.93%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,40 +1,42 @@
 class Solution {
-    public boolean hasCycle(int i, List<List<Integer>> adjacency, boolean[] visited, boolean[] stack){
-        visited[i] = true;
-        stack[i] = true;
-
-        for(int n : adjacency.get(i)){
-            if(stack[n] || (!visited[n] && hasCycle(n, adjacency, visited, stack))){
-                return true;
-            }
-        }
-        stack[i] = false;
-        return false;
-    }
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        if(prerequisites.length == 1 || numCourses == 1 || prerequisites.length == 0){
-            return true;
-        }
-
-        boolean[] visited = new boolean[numCourses];
-        boolean[] stack = new boolean[numCourses];
-        
-        List<List<Integer>> adjacency = new ArrayList<>(numCourses);
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        int[] inDegree = new int [numCourses];
+        int count = 0;
 
         for(int i = 0; i < numCourses; i++){
-            adjacency.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
         }
 
-        for(int[] p : prerequisites){
-            adjacency.get(p[0]).add(p[1]);
+        for(int i = 0; i < prerequisites.length; i++){
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
 
         for(int i = 0; i < numCourses; i++){
-            if(!visited[i] && hasCycle(i, adjacency, visited, stack)){
-                return false;
+            for(int n : adj.get(i)){
+                inDegree[n]++;
             }
         }
 
-        return true;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < inDegree.length; i++){
+            if(inDegree[i] == 0){
+                q.add(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            count++;
+
+            for(int n : adj.get(node)){
+                inDegree[n]--;
+                if(inDegree[n] == 0){
+                    q.add(n);
+                }
+            }
+        }
+
+        return count == numCourses;
     }
 }
