ef7bb10c3d0f0272b33180e65e45ea8f6022b5df
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-02-01 22:34:33 -0500

    Time: 0 ms (100.00%) | Memory: 41.3 MB (50.38%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -3,16 +3,14 @@ public class Solution {
         int n = nums.length;
         boolean rotated = false;
         
-        // Find the breaking point where rotation occurs
         for (int i = 1; i < n; i++) {
             if (nums[i] < nums[i - 1]) {
-                // If breaking point is not the only point where nums[i] < nums[i-1], return false
+                
                 if (rotated) return false;
                 rotated = true;
             }
         }
         
-        // Check wrap-around condition if rotated
         if (rotated && nums[n - 1] > nums[0]) {
             return false;
         }
