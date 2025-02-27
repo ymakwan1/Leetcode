3de643215e080f7a504708d938ad5b771ebf41d4
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-02-26 19:35:04 -0500

    Time: 1 ms (88.49%) | Memory: 44.6 MB (99.79%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,28 +1,64 @@
+class DS{
+    private int[] parent;
+    private int[] size;
+
+    public DS(int n){
+        parent = new int[n];
+        size = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int find(int u){
+        if(u == parent[u]){
+            return u;
+        }
+
+        return parent[u] = find(parent[u]);
+    }
+
+    public void unionBySize(int x, int y){
+        int rootX = find(x);
+        int rootY = find(y);
+
+        if(rootX != rootY){
+            if(size[rootX] < size[rootY]){
+                parent[rootX] = rootY;
+                size[rootY] += size[rootX];
+            }else{
+                parent[rootY] = rootX;
+                size[rootX] += size[rootY];
+            }
+        }
+    }
+
+    public int countProvinces(){
+        int count = 0;
+        for(int i = 0; i < parent.length; i++){
+            if(parent[i] == i){
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-
-        boolean[] visited = new boolean[n];
-        int provinces = 0;
+        DS ds = new DS(n);
 
         for(int i = 0; i < n; i++){
-            if(!visited[i]){
-                dfs(isConnected, visited, i);
-                provinces++;
+            for(int j = i+1; j < n; j++){
+                if(isConnected[i][j] == 1){
+                    ds.unionBySize(i, j);
+                }
             }
         }
 
-        return provinces;
-    }
-
-    private void dfs(int[][] isConnected, boolean[] visited, int city){
-        if(visited[city])
-            return;
-        visited[city] = true;
-        for(int n = 0; n < isConnected.length; n++){
-            if(isConnected[city][n] == 1 && !visited[n]){
-                dfs(isConnected, visited, n);
-            }
-        }
+        return ds.countProvinces();
     }
 }
