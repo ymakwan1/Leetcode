d18ffc8a55e7db201953948088b1edd7b2104e80
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-03-07 17:15:29 -0500

    Time: 0 ms (100%), Space: 42.1 MB (88.12%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -3,34 +3,34 @@ class Solution {
         int n = matrix.length;
 
         //Brute
-        int[][] ans = new int[n][n];
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                ans[j][n -1 - i] = matrix[i][j];
+        // int[][] ans = new int[n][n];
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < n; j++){
+        //         ans[j][n -1 - i] = matrix[i][j];
+        //     }
+        // }
+        // for (int i = 0; i < ans.length; i++) {
+        //     matrix[i] = Arrays.copyOf(ans[i], ans[i].length);
+        // }
+
+        for(int row = 0; row < n; row++){
+            for(int col = row; col < n; col++){
+                if(row != col){
+                    int temp = 0;
+                    temp = matrix[row][col];
+                    matrix[row][col] = matrix[col][row];
+                    matrix[col][row] = temp;
+                }
             }
         }
-        for (int i = 0; i < ans.length; i++) {
-            matrix[i] = Arrays.copyOf(ans[i], ans[i].length);
+
+        for(int row = 0; row < n; row++){
+            for(int col = 0; col < n/2; col++){
+                int temp = 0;
+                temp = matrix[row][col];
+                matrix[row][col] = matrix[row][n-col-1];
+                matrix[row][n-col-1] = temp;
+            }
         }
-
-        // for(int row = 0; row < n; row++){
-        //     for(int col = row; col < n; col++){
-        //         if(row != col){
-        //             int temp = 0;
-        //             temp = matrix[row][col];
-        //             matrix[row][col] = matrix[col][row];
-        //             matrix[col][row] = temp;
-        //         }
-        //     }
-        // }
-
-        // for(int row = 0; row < n; row++){
-        //     for(int col = 0; col < n/2; col++){
-        //         int temp = 0;
-        //         temp = matrix[row][col];
-        //         matrix[row][col] = matrix[row][n-col-1];
-        //         matrix[row][n-col-1] = temp;
-        //     }
-        // }
     }
 }
