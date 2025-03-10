0a198b28b1237a6c8a77875626434e970f3416af
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-03-09 20:48:17 -0400

    Time: 16 ms (10.87%), Space: 48.1 MB (56.3%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,23 +3,34 @@ class Solution {
         int n = nums.length;
         List<Integer> result = new ArrayList<>();
         //Brute
+        // for(int i = 0; i < n; i++){
+        //     if(result.size() == 0 || result.get(0) != nums[i]){
+        //         int count = 0;
+        //         for(int j = 0; j < n; j++){
+        //             if(nums[j] == nums[i]){
+        //                 count++;
+        //             } 
+        //         }
+        //         if(count > (n/3)){
+        //             result.add(nums[i]);
+        //         }
+        //         if(result.size() == 2){
+        //             break;
+        //         }
+        //     }
+        // }
+
+        //Better
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int minValue = (int)(Math.floor(n/3) + 1);
+
         for(int i = 0; i < n; i++){
-            if(result.size() == 0 || result.get(0) != nums[i]){
-                int count = 0;
-                for(int j = 0; j < n; j++){
-                    if(nums[j] == nums[i]){
-                        count++;
-                    } 
-                }
-                if(count > (n/3)){
-                    result.add(nums[i]);
-                }
-                if(result.size() == 2){
-                    break;
-                }
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+            if(map.get(nums[i]) == minValue){
+                result.add(nums[i]);
             }
         }
-        //Better
+    
 
         //Optimal
         //potential candidates
