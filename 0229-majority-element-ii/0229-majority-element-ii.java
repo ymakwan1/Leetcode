17fba592153f273b0e3788577d9f3418b9b425bd
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-03-09 20:48:49 -0400

    Time: 2 ms (99.14%), Space: 48.3 MB (41%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -21,50 +21,50 @@ class Solution {
         // }
 
         //Better
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int minValue = (int)(Math.floor(n/3) + 1);
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // int minValue = (int)(Math.floor(n/3) + 1);
 
-        for(int i = 0; i < n; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-            if(map.get(nums[i]) == minValue){
-                result.add(nums[i]);
-            }
-        }
+        // for(int i = 0; i < n; i++){
+        //     map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        //     if(map.get(nums[i]) == minValue){
+        //         result.add(nums[i]);
+        //     }
+        // }
     
 
         //Optimal
         //potential candidates
-        // int candidate1 = Integer.MIN_VALUE, candidate2 = Integer.MIN_VALUE;
-        // int count1 = 0, count2 = 0;
+        int candidate1 = Integer.MIN_VALUE, candidate2 = Integer.MIN_VALUE;
+        int count1 = 0, count2 = 0;
 
-        // for (int num : nums) {
-        //     if (num == candidate1) {
-        //         count1++;
-        //     } else if (num == candidate2) {
-        //         count2++;
-        //     } else if (count1 == 0) {
-        //         candidate1 = num;
-        //         count1 = 1;
-        //     } else if (count2 == 0) {
-        //         candidate2 = num;
-        //         count2 = 1;
-        //     } else {
-        //         count1--;
-        //         count2--;
-        //     }
-        // }
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
 
-        // //Verify if they are valid majority elements
-        // count1 = 0;
-        // count2 = 0;
-        // for (int num : nums) {
-        //     if (num == candidate1) count1++;
-        //     else if (num == candidate2) count2++;
-        // }
+        //Verify if they are valid majority elements
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
 
         
-        // if (count1 > n / 3) result.add(candidate1);
-        // if (count2 > n / 3) result.add(candidate2);
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
 
         return result;
     }
