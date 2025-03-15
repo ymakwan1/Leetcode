4c16acab070b4c125665712be4cd981ed7ebe381
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-03-15 17:07:58 -0400

    Time: 29 ms (72.23%), Space: 51.8 MB (43.78%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -19,21 +19,50 @@ class Solution {
         // result = new ArrayList<>(set);
 
         //Better
-        HashSet<List<Integer>> set = new HashSet<>();
-        HashSet<Integer> st = new HashSet<>();
+        // HashSet<List<Integer>> set = new HashSet<>();
+        // HashSet<Integer> st = new HashSet<>();
+        // for(int i = 0; i < n; i++){
+        //     st = new HashSet<>();
+        //     for(int j = i+1; j < n; j++){
+        //         int k = -(nums[i] + nums[j]);
+        //         if(st.contains(k)){
+        //             List<Integer> temp = Arrays.asList(nums[i], nums[j], k);
+        //             temp.sort(null);
+        //             set.add(temp);
+        //         }
+        //         st.add(nums[j]);
+        //     }
+        // }
+        // result = new ArrayList<>(set);
+
+        //Optimal
+        result = new ArrayList<>();
+        Arrays.sort(nums);
         for(int i = 0; i < n; i++){
-            st = new HashSet<>();
-            for(int j = i+1; j < n; j++){
-                int k = -(nums[i] + nums[j]);
-                if(st.contains(k)){
-                    List<Integer> temp = Arrays.asList(nums[i], nums[j], k);
-                    temp.sort(null);
-                    set.add(temp);
+            if(i > 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int j = i + 1;
+            int k = n - 1;
+
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum > 0){
+                    k--;
+                }else if(sum < 0){
+                    j++;
+                }else{
+                    result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
                 }
-                st.add(nums[j]);
             }
         }
-        result = new ArrayList<>(set);
+
         return result;
     }
 }
