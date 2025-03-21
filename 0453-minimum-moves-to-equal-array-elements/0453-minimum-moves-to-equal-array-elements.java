7ae9d77080cacdca13dedbd4e282cbc2c327acaf
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-03-21 16:17:51 -0400

    Time: 2 ms (89.93%), Space: 46.4 MB (5.04%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.java
@@ -1,11 +1,20 @@
 class Solution {
     public int minMoves(int[] nums) {
         //Better
-        Arrays.sort(nums);
-        int moves = 0;
-        for(int i = 1; i < nums.length; i++){
-            moves += (nums[i] - nums[0]);
+        // Arrays.sort(nums);
+        // int moves = 0;
+        // for(int i = 1; i < nums.length; i++){
+        //     moves += (nums[i] - nums[0]);
+        // }
+        // return moves;
+
+        //Optimal
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for(int n : nums){
+            sum += n;
+            min = Math.min(min, n);
         }
-        return moves;
+        return sum - (min * nums.length);
     }
 }
