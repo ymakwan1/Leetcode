3322a734034087b525218cf1e42cb73b48307e73
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-03-24 15:24:49 -0400

    Time: 47 ms (76.84%) | Memory: 97.5 MB (8.97%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -0,0 +1,29 @@
+class Solution {
+    static class Meeting{
+        int startTime, endTime;
+        Meeting(int startTimeIn, int endTimeIn){
+            startTime = startTimeIn;
+            endTime = endTimeIn;
+        }
+    }
+    public int eraseOverlapIntervals(int[][] intervals) {
+        int n = intervals.length;
+        
+        Meeting[] meeting = new Meeting[n];
+        for(int i = 0; i < n; i++){
+            meeting[i] = new Meeting(intervals[i][0], intervals[i][1]);
+        }
+        Arrays.sort(meeting, (a, b) -> a.endTime - b.endTime);
+
+        int count = 1, freeTime = meeting[0].endTime;
+        
+        for(int i = 1; i < n; i++){
+            if(meeting[i].startTime >= freeTime){
+                count++;
+                freeTime = meeting[i].endTime;
+            }
+        }
+
+        return n - count;
+    }
+}
