c443c3d70998953ac604af901d1a038e649810a3
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-03-25 15:08:19 -0400

    Time: 15 ms (30.49%), Space: 42.7 MB (23.97%) - LeetHub

--- 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.java
+++ 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.java
@@ -3,14 +3,29 @@ class Solution {
         int n = word.length();
         int count = 0;
         //Brute
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i; j < n; j++){
+        //         if(isValid(word.substring(i, j + 1))){
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        //Better
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
         for(int i = 0; i < n; i++){
+            Set<Character> present = new HashSet<>();
             for(int j = i; j < n; j++){
-                if(isValid(word.substring(i, j + 1))){
+                char c = word.charAt(j);
+                if(!vowels.contains(c)){
+                    break;
+                }
+                present.add(c);
+                if(present.size() == 5){
                     count++;
                 }
             }
         }
-
         return count;
     }
 
