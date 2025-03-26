55501960a6d1e467f1af23242fc4e26d5a1c9003
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-03-26 19:22:52 -0400

    Time: 1 ms (12.57%) | Memory: 42.8 MB (48.10%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        // if(root == null){
+        //     return 0;
+        // }
+        
+        // int lhs = maxDepth(root.left);
+        // int rhs = maxDepth(root.right);
+        
+        // return Math.max(lhs, rhs) + 1;
+        //List<List<Integer>> listOfLists = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        int level = 0;
+        if(root == null){
+            return 0;
+        }
+        
+        queue.offer(root);
+        
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            // List<Integer> currentLevel = new ArrayList<>();
+
+            for(int i = 0; i < levelSize; i++){
+                TreeNode temp = queue.poll();
+                //currentLevel.add(temp.val);
+                if(temp.left != null){
+                    queue.offer(temp.left);
+                }
+                
+                if(temp.right != null){
+                    queue.offer(temp.right);
+                }
+            }
+            //listOfLists.add(currentLevel);
+            level++;
+        }
+        return level;
+    }
+}
