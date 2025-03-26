839a96a7b64b7b9dfc1d9a9d11ef57c0c1441316
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-03-26 19:47:04 -0400

    Time: 0 ms (100%), Space: 44.9 MB (7.9%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -20,15 +20,7 @@ class Solution {
             return true;
         }
         
-        int left = getHeight(root.left);
-        int right = getHeight(root.right);
-        
-        if(Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right)){
-            return true;
-        }
-        
-        return false;
-        
+        return getHeight(root) != -1;
     }
     
     public int getHeight(TreeNode node){
@@ -37,7 +29,17 @@ class Solution {
         }
         
         int lhs = getHeight(node.left);
+        if(lhs == -1){
+            return -1;
+        }
         int rhs = getHeight(node.right);
+        if(rhs == -1){
+            return -1;
+        }
+
+        if(Math.abs(lhs - rhs) > 1){
+            return -1;
+        }
         
         return Math.max(lhs, rhs) + 1;
     }
