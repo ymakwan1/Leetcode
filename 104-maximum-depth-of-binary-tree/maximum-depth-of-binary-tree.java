a1b547f975daf313ff1e5895c6edf75970d70198
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-03-26 19:24:16 -0400

    Time: 0 ms (100.00%) | Memory: 42.7 MB (48.10%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -16,39 +16,39 @@
 class Solution {
     public int maxDepth(TreeNode root) {
         // Recursion
-        // if(root == null){
-        //     return 0;
-        // }
-        
-        // int lhs = maxDepth(root.left);
-        // int rhs = maxDepth(root.right);
-        
-        // return Math.max(lhs, rhs) + 1;
-
-        // Level Order
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-        int level = 0;
         if(root == null){
             return 0;
         }
         
-        queue.offer(root);
+        int lhs = maxDepth(root.left);
+        int rhs = maxDepth(root.right);
         
-        while(!queue.isEmpty()){
-            int levelSize = queue.size();
+        return Math.max(lhs, rhs) + 1;
 
-            for(int i = 0; i < levelSize; i++){
-                TreeNode temp = queue.poll();
-                if(temp.left != null){
-                    queue.offer(temp.left);
-                }
+        // Level Order
+        // Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        // int level = 0;
+        // if(root == null){
+        //     return 0;
+        // }
+        
+        // queue.offer(root);
+        
+        // while(!queue.isEmpty()){
+        //     int levelSize = queue.size();
+
+        //     for(int i = 0; i < levelSize; i++){
+        //         TreeNode temp = queue.poll();
+        //         if(temp.left != null){
+        //             queue.offer(temp.left);
+        //         }
                 
-                if(temp.right != null){
-                    queue.offer(temp.right);
-                }
-            }
-            level++;
-        }
-        return level;
+        //         if(temp.right != null){
+        //             queue.offer(temp.right);
+        //         }
+        //     }
+        //     level++;
+        // }
+        // return level;
     }
 }
