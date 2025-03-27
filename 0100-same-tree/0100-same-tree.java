9308a721af382a0947686f78ca826a2e2084468e
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-03-26 20:46:16 -0400

    Time: 0 ms (100%), Space: 41.4 MB (28.73%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,18 +15,10 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null){
-            return true;
-        }
-        
         if(p == null || q == null){
-            return false;
+            return p == q;
         }
         
-        if(p.val != q.val){
-            return false;
-        }
-        
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
