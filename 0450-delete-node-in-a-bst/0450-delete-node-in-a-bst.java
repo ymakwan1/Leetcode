55d1436e6550eb7aee1b79ec89a17018774b5a34
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-04-07 18:39:09 -0400

    Time: 0 ms (100%), Space: 46 MB (77.45%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -19,37 +19,51 @@ class Solution {
             return null;
         }
 
-        if(key < root.val){
-            root.left = deleteNode(root.left, key);
-        }else if(key > root.val){
-            root.right = deleteNode(root.right, key);
-        }else{
-            //No child
-            if(root.left == null && root.right == null){
-                return null;
-            }
-
-            //Node has one child
-            if(root.left == null){
-                return root.right;
-            }else if(root.right == null){
-                return root.left;
-            }
-
-            //Node has two child
-            TreeNode successor = findMin(root.right);
-            root.val = successor.val;
-            root.right = deleteNode(root.right, successor.val);
+        if(root.val == key){
+            return helper(root);
         }
 
-        return root;
+        TreeNode dummy = root;
+
+        while(root != null){
+            if(root.val > key){
+                if(root.left != null && root.left.val == key){
+                    root.left = helper(root.left);
+                    break;
+                }else{
+                    root = root.left;
+                }
+            }else{
+                if(root.right != null && root.right.val == key){
+                    root.right = helper(root.right);
+                    break;
+                }else{
+                    root = root.right;
+                }
+            }
+        }
+
+        return dummy;
     }
 
-    private TreeNode findMin(TreeNode node) {
-        // Find the minimum node in a given subtree
-        while (node.left != null) {
-            node = node.left;
+    private TreeNode helper(TreeNode root){
+        if(root.left == null){
+            return root.right;
+        }else if(root.right == null){
+            return root.left;
+        } else{
+            TreeNode rightChild = root.right;
+            TreeNode lastRight = findLastRight(root.left);
+            lastRight.right = rightChild;
+            return root.left;
         }
-        return node;
+    }
+
+    private TreeNode findLastRight(TreeNode root){
+        if(root.right == null){
+            return root;
+        }
+
+        return findLastRight(root.right);
     }
 }
