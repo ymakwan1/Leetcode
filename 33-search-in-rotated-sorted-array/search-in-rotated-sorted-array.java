a0d0e602a9012664b2c13e40d8e1bbcc7104466f
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-04-23 18:36:39 -0400

    Time: 0 ms (100.00%) | Memory: 41.8 MB (97.17%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,28 +1,34 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
+        //Brute  - linear search
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] == target) {
+        //         return i ;
+        //     }
+        // }
+        // return -1;
 
-        while(low <= high){
-            int mid = (low + high) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }
+        // Better - binary search
 
-            //left sorted
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && nums[mid] >= target){
-                    high = mid - 1;
-                }else {
-                    low = mid + 1;
+        int left = 0, right  = nums.length - 1;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+            
+            if(nums[mid] == target) return mid;
+
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && target < nums[mid]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
                 }
             }
-            //right sorted 
-            else {
-                if(nums[high] >= target && nums[mid] <= target){
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
+            else{
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
                 }
             }
         }
