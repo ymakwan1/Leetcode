56aae4c26a8e9f8c98dcc7bc609a5ab60e20e9ca
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-04-23 17:26:01 -0400

    Time: 1 ms (99.53%), Space: 40.6 MB (98.59%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.java
@@ -1,42 +1,39 @@
 class Solution {
     public String fractionToDecimal(int numerator, int denominator) {
-        StringBuilder result = new StringBuilder();
-        HashMap<Long, Integer> map = new HashMap<>();
-
         if(numerator == 0){
             return "0";
         }
 
-        if((numerator < 0) ^ (denominator < 0)){
+        StringBuilder result = new StringBuilder();
+
+        if ((numerator < 0) ^ (denominator < 0)){
             result.append("-");
         }
 
-        long num = Math.abs((long)numerator);
-        long den = Math.abs((long)denominator);
+        long num = Math.abs((long) numerator);
+        long den = Math.abs((long) denominator);
 
-        result.append(String.valueOf(num/den));
-
-        long remainder = num%den;
+        result.append(num / den);
+        long remainder = num % den;
 
         if(remainder == 0){
             return result.toString();
         }
-
         result.append(".");
+        HashMap<Long, Integer> map = new HashMap<>();
 
         while(remainder != 0){
             if(map.containsKey(remainder)){
-                int index = map.get(remainder);
-                result.insert(index, "(");
+                result.insert(map.get(remainder), "(");
                 result.append(")");
                 break;
             }
-
             map.put(remainder, result.length());
             remainder *= 10;
             result.append(remainder / den);
             remainder %= den;
         }
+
         return result.toString();
     }
 }
