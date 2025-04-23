e5eefdecf4f0b8a8243d16de7623e8e7d792892a
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-04-23 17:40:43 -0400

    Time: 1 ms (99.51%), Space: 44.2 MB (53.17%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,30 +1,49 @@
 class Solution {
     public int compress(char[] chars) {
         //Brute - StringBuilder extra space of O(N)
-        StringBuilder sb = new StringBuilder();
-        int index = 0;
+        // StringBuilder sb = new StringBuilder();
+        // int index = 0;
 
-        while(index < chars.length){
-            char currentCharacter = chars[index];
-            int count = 0;
+        // while(index < chars.length){
+        //     char currentCharacter = chars[index];
+        //     int count = 0;
 
-            while (index < chars.length && chars[index] == currentCharacter) {
-                count++;
-                index++;
-            }
+        //     while (index < chars.length && chars[index] == currentCharacter) {
+        //         count++;
+        //         index++;
+        //     }
 
-            sb.append(currentCharacter);
-            if (count > 1) {
-                sb.append(count);
+        //     sb.append(currentCharacter);
+        //     if (count > 1) {
+        //         sb.append(count);
+        //     }
+        // }
+
+        // for (int i = 0; i < sb.length(); i++) {
+        //     chars[i] = sb.charAt(i);
+        // }
+
+        // return sb.length();
+
+        //Better - Two pointer
+        int n = chars.length;
+        int writeIndex = 0;
+        int i = 0;
+
+        for (int j = 0; j < n; j++) {
+            if(j == n - 1 || chars[j] != chars[j + 1]){
+                chars[writeIndex++] = chars[i];
+
+                int count = j - i + 1;
+                if (count > 1) {
+                    for (char c : String.valueOf(count).toCharArray()) {
+                        chars[writeIndex++] = c;
+                    }
+                }
+                i = j + 1;
             }
         }
 
-        for (int i = 0; i < sb.length(); i++) {
-            chars[i] = sb.charAt(i);
-        }
-
-        return sb.length();
-
-        //Better
+        return writeIndex;
     }
 }
