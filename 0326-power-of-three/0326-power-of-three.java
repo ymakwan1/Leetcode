ea45c82280eeb076a769af79046ffc58df50ef47
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-04-24 18:21:03 -0400

    Time: 8 ms (87.57%), Space: 44.7 MB (6.68%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,14 +1,24 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
         //Brute multiply and check
+        // if(n < 1){
+        //     return false;
+        // }
+        // long power = 1;
+        // while(power < n){
+        //     power *= 3;
+        // }
+
+        // return power == n;
+
+        //Better - divide by 3 repeatedly
         if(n < 1){
             return false;
         }
-        long power = 1;
-        while(power < n){
-            power *= 3;
+        while(n % 3 == 0){
+            n /= 3;
         }
 
-        return power == n;
+        return n == 1;
     }
 }
