1d166638dda333e2d73606c68fd9497901c4f90d
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-04-24 20:21:33 -0400

    Time: 1 ms (85.44%) | Memory: 40.7 MB (93.29%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0 || x == 1){
+            return x;
+        }
+
+        int left = 1, right = x / 2, ans = 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(mid <= (x / mid)){
+                ans = mid;
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
