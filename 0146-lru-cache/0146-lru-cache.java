2af71a2109c7942192b1e662c4a68792d8a5d998
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-04-26 15:48:09 -0400

    Time: 46 ms (62.5%), Space: 113.1 MB (68.53%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,23 +1,65 @@
 class LRUCache {
+    class Node {
+        int key, value;
+        Node prev, next;
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
-    private LinkedHashMap<Integer, Integer> cache;
-    
+    private Map<Integer, Node> map;
+    private Node head, tail;
+    private int capacity;
+
     public LRUCache(int capacity) {
-        this.cache = new LinkedHashMap<>(capacity, 0.75f, true) {
-            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-                return size() > capacity;
-            }
-        };
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
     }
-    
+
     public int get(int key) {
-        return cache.getOrDefault(key, -1);
+        if (!map.containsKey(key)) return -1;
+        Node node = map.get(key);
+        remove(node);
+        insertAtHead(node);
+        return node.value;
     }
-    
+
     public void put(int key, int value) {
-        cache.put(key, value);
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            remove(node);
+            insertAtHead(node);
+        } else {
+            if (map.size() == capacity) {
+                Node lru = tail.prev;
+                remove(lru);
+                map.remove(lru.key);
+            }
+            Node newNode = new Node(key, value);
+            insertAtHead(newNode);
+            map.put(key, newNode);
+        }
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insertAtHead(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
     }
 }
+
 
 /**
  * Your LRUCache object will be instantiated and called as such:
