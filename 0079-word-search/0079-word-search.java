0ba3b7355cb75621c2d0fc797d0d09c50ee366b4
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-04-30 14:17:45 -0400

    Time: 149 ms (50.57%), Space: 41.6 MB (78.27%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,39 +1,44 @@
-class Solution {
+public class Solution {
     public boolean exist(char[][] board, String word) {
         int rows = board.length;
         int cols = board[0].length;
-        if(rows * cols < word.length()){
+        
+        if (rows * cols < word.length()) {
             return false;
         }
+
+        boolean[][] visited = new boolean[rows][cols];
+
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
-                if(dfs(r,c,board,word,0)){
+                if (dfs(r, c, board, word, 0, visited)) {
                     return true;
                 }
             }
         }
+
         return false;
     }
 
-    public boolean dfs(int r,int c,char[][] board, String word, int index){
-        if(index == word.length()){
+    private boolean dfs(int r, int c, char[][] board, String word, int index, boolean[][] visited) {
+        if (index == word.length()) {
             return true;
         }
-        
-        if(r < 0 || r >= board.length || c < 0 || c >= board[0].length || board[r][c] != word.charAt(index)){
+
+        if (r < 0 || r >= board.length || c < 0 || c >= board[0].length ||
+            board[r][c] != word.charAt(index) || visited[r][c]) {
             return false;
         }
 
-        
+        visited[r][c] = true;
 
-        char temp = board[r][c];
-        board[r][c] = '.';
+        boolean found = dfs(r + 1, c, board, word, index + 1, visited) ||
+                        dfs(r - 1, c, board, word, index + 1, visited) ||
+                        dfs(r, c + 1, board, word, index + 1, visited) ||
+                        dfs(r, c - 1, board, word, index + 1, visited);
 
-        boolean found = dfs(r+1, c, board, word, index+1) ||
-                        dfs(r-1, c, board, word, index+1) ||
-                        dfs(r, c+1, board, word, index+1) ||
-                        dfs(r, c-1, board, word, index+1);
-        board[r][c] = temp;
+        visited[r][c] = false; // Backtrack
+
         return found;
     }
 }
