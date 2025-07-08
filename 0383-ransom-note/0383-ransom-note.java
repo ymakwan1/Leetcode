c73f82db1ad033a1992b64cf1cc20336a681bc3b
ymakwan1 <111987838+ymakwan1@users.noreply.github.com>
2025-07-07 20:24:03 -0400

    Time: 2 ms (80.96%), Space: 45.1 MB (55.46%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,28 +1,16 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> mag = new HashMap<>();
+        int[] freq = new int[26];
 
-        for(char m : magazine.toCharArray()){
-            if(!mag.containsKey(m)){
-                mag.put(m, 1);
-            } else {
-                int val = mag.get(m);
-                mag.put(m, val + 1);
-            }
+        for (char c : magazine.toCharArray()) {
+            freq[c - 'a']++;
         }
-        for(char r : ransomNote.toCharArray()){
-            if(mag.containsKey(r)){
-                int count = mag.get(r);
 
-                if(count > 0){
-                    mag.put(r, count - 1);
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
+        for (char c : ransomNote.toCharArray()) {
+            freq[c - 'a']--;
+            if (freq[c - 'a'] < 0) return false;
         }
+
         return true;
     }
 }
